19ffb3455fcef62e501fe8726f1308759936ecb4
Fran Nav <frannavmart@gmail.com>
2021-03-06 19:04:57 +0000

    Added new LeagueItem component

---
+++ src/components/Leagues/LeagueItem.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  Text,
+  Image,
+  View,
+  StyleSheet
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+  },
+
+  imgWrapper: {
+
+  },
+
+  textWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignContent: 'center',
+    padding: 20,
+  }
+
+})
+
+const LeagueItem = ({ name, img }) => {
+
+  return (
+    <>
+    <View style={styles.container}>
+      <View>
+        <Image
+          source={{ uri:  `${img["Logo de la Liga"]}`}}
+          style={{ width: 70, height: 70 }}
+        />
+      </View>
+      <View style={styles.textWrapper}>
+        <Text>
+          {name["Nombre De La Liga"]}
+        </Text>
+      </View>
+    </View>
+    </>
+  );
+};
+
+
+
+export default LeagueItem;
