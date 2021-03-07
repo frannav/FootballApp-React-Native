8bd7d55ffbc66c75a10a95dcd660a6d0aa255fbb
Fran Nav <frannavmart@gmail.com>
2021-03-07 17:56:28 +0000

    AddPlayer working with jsonDB ✅

--- src/components/Players/AddPlayer.js
+++ src/components/Players/AddPlayer.js
@@ -4,17 +4,50 @@ import {
   View,
   Text,
   TextInput,
-  StyleSheet
+  Button
 } from 'react-native';
 
-const styles = StyleSheet.create({
-})
+
+const DEFAULT_AVATAR = 'https://robohash.org/inutminus.png?size=250x250&set=set1'
+const DEFAULT_TEAMID = ''
 
 const AddPlayer = () => {
 
-  const [ value, onChangeText ] = useState(null)
+  const [ name, onChangeText ] = useState(null)
+  const [ teamId, onChangeTeam ] = useState(DEFAULT_TEAMID)
+  const [ avatar, onChangeAvatar ] = useState(DEFAULT_AVATAR)
+  const [ isOk, setIsOk ] = useState(null)
+  const [ error, setError ] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  console.log(value)
+  let dataToSend = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ 
+      "Nombre del Jugador": `${name}`,
+      "teamId": `${teamId}`,
+      "Avatar": `${avatar}`
+    })
+  }
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    fetch('http://localhost:3000/players', dataToSend)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true)
+        },
+        (error) => {
+          setIsLoaded(true)
+          setError(error)
+        }
+      )
+    onChangeText(null)
+    setIsOk(true)
+  }
+  
   return (
     <>
       <View>
@@ -27,10 +60,35 @@ const AddPlayer = () => {
           </Text>
           <TextInput
             style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-            onChangeText={text => onChangeText(text)}
-            value={value}
+            onChangeText={text => onChangeText(text) & setIsOk(false)}
+            name={name}
+            clearButtonMode='always'
+          />
+          <Text>
+            teamId:
+          </Text>
+          <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            onChangeText={text => onChangeTeam(text) & setIsOk(false)}
+            name={teamId}
+            clearButtonMode='always'
+          />
+          <Text>
+            Avatar:
+          </Text>
+          <TextInput
+            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+            onChangeText={text => onChangeTeam(text) & setIsOk(false)}
+            name={teamId}
+            clearButtonMode='always'
           />
         </View>
+        <Button
+          title='Pulsa para añadir el nuevo jugador'
+          onPress={handleSubmit}
+        />
+        { isOk ? <Text>Enviado</Text> : null }
+        { error ? <Text>Error: {error.message}</Text> : null }
       </View>
     </>
   );
