9a9ee634df171156cab4b79423699e39f5b9140e
Fran Nav <frannavmart@gmail.com>
2021-03-07 16:56:02 +0000

    Added New Component AddPlayer

---
+++ src/components/Players/AddPlayer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+} from 'react-native';
+
+const styles = StyleSheet.create({
+})
+
+const AddPlayer = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+
+
+export default AddPlayer;
