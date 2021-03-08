330fa9295df0f4260aba85018289a69952bb4718
Fran Nav <frannavmart@gmail.com>
2021-03-08 13:09:34 +0000

    Delete unused imports

--- App.js
+++ App.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import {
-  StyleSheet,
-} from 'react-native';
-
-
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createStackNavigator } from '@react-navigation/stack'
-import HomeScreen from './src/views/HomeScreen.js'
-// import LeaguesScreen from './src/views/LeaguesScreen.js'
 import LeagueStack from './src/components/Leagues/LeagueStack.js'
-// import TeamsScreen from './src/views/TeamsScreen.js'
-import PlayersScreen from './src/views/PlayersScreen.js'
 import TeamStack from './src/components/Teams/TeamStack.js';
 import PlayerStack from './src/components/Players/PlayerStack.js';
 
@@ -80,7 +71,6 @@ const App = () => {
             headerShown: false
           }}
         >
-          {/* <Tab.Screen name='Inicio' component={HomeScreen} /> */}
           <Tab.Screen name='Ligas' component={LeaguesStackScreens} />
           <Tab.Screen name='Equipos' component={TeamsStackScreens} />
           <Tab.Screen name='Jugadores' component={PlayersStackScreen} />
