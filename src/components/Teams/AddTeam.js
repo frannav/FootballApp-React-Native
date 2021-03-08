085595c72e28f1db2d972a83e5ce86b47edab10b
Fran Nav <frannavmart@gmail.com>
2021-03-08 11:41:49 +0000

    Styling addTeam form

--- src/components/Teams/AddTeam.js
+++ src/components/Teams/AddTeam.js
@@ -4,8 +4,36 @@ import {
   View,
   Text,
   TextInput,
-  Button
+  Button,
+  StyleSheet
 } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 25,
+    paddingBottom: 200,
+    backgroundColor: '#d8e2dc'
+  },
+
+  inputContainer: {
+    paddingTop: 28,
+    paddingBottom: 28,
+  },
+
+  title: {
+    fontSize: 20,
+    paddingBottom: 70,
+  },
+
+  text: {
+    fontSize: 20,
+    padding: 10,
+  },
+
+  buttom: {
+    paddingTop: 30,
+  },
+})
 
 
 const DEFAULT_LOGO = 'https://robohash.org/inutminus.png?size=250x250&set=set1'
@@ -47,42 +75,49 @@ const AddTeam = () => {
   
   return (
     <>
-      <View>
-        <Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>
           Añadir Jugador:
         </Text>
         <View>
-          <Text>
-            Nombre:
-          </Text>
-          <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-            onChangeText={text => onChangeText(text) & setIsOk(false)}
-            name={name}
-            clearButtonMode='always'
-          />
-          <Text>
-          leagueId:
-          </Text>
-          <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-            onChangeText={text => onChangeLeague(text) & setIsOk(false)}
-            name={leagueId}
-            clearButtonMode='always'
-          />
-          <Text>
-            Avatar:
-          </Text>
-          <TextInput
-            style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-            onChangeText={text => onChangeLogo(text) & setIsOk(false)}
-            name={logo}
-            clearButtonMode='always'
-          />
+          <View style={styles.inputContainer}>
+            <Text style={styles.text}>
+              Nombre:
+            </Text>
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: '#e0fbfc' }}
+              onChangeText={text => onChangeText(text) & setIsOk(false)}
+              name={name}
+              clearButtonMode='always'
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.text}>
+            leagueId:
+            </Text>
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: '#e0fbfc' }}
+              onChangeText={text => onChangeLeague(text) & setIsOk(false)}
+              name={leagueId}
+              clearButtonMode='always'
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.text}>
+              Avatar:
+            </Text>
+            <TextInput
+              style={{ height: 40, borderColor: 'gray', borderWidth: 1, backgroundColor: '#e0fbfc' }}
+              onChangeText={text => onChangeLogo(text) & setIsOk(false)}
+              name={logo}
+              clearButtonMode='always'
+            />
+          </View>
         </View>
         <Button
           title='Pulsa para añadir el nuevo equipo'
           onPress={handleSubmit}
+          style={styles.buttom}
         />
         { isOk ? <Text>Enviado</Text> : null }
         { error ? <Text>Error: {error.message}</Text> : null }
